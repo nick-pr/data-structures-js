6fc0c1c366de66fa35b62b262e6e289a1d9789e0
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-01-14 12:29:04 -0500

    started on BST

---
+++ bst/bst.js
@@ -0,0 +1,32 @@
+import Node from './node.js'
+
+class BinarySearchTree {
+    constructor(node) {
+        this.root = node
+    }
+
+    insert(key, value) {
+        const newNode = new Node(key, value)
+        if (!this.root) return (this.root = newNode)
+
+        const traverse = (node) => {
+            if (newNode.key === node.key) return
+            if (newNode.key > node.key) {
+                if (!node.right) return (node.right = newNode)
+                traverse(node.right)
+            }
+            if (newNode.key < node.key) {
+                if (!node.left) return (node.left = newNode)
+                traverse(node.left)
+            }
+        }
+        traverse(this.root)
+    }
+}
+
+const bst = new BinarySearchTree()
+bst.insert(5, 'hello')
+bst.insert(4, 'hello')
+bst.insert(3, 'hello')
+bst.insert(10, 'hello')
+console.log(bst)
---
+++ bst/node.js
@@ -0,0 +1,8 @@
+export default class Node {
+    constructor(key, value) {
+        this.key = key
+        this.value = value
+        this.left
+        this.right
+    }
+}
