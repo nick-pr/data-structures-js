409cba32a0d81e98ae04f45c7b203ae1e0ec8e37
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-01-15 20:43:56 -0500

    Fixed some comments.

--- bst/bst.js
+++ bst/bst.js
@@ -7,41 +7,46 @@ class BinarySearchTree {
     /** Given a key and value, will insert that key and it's associated value in the tree.
      * Utilizes recursion to traverse the tree.*/
     insert(key, value) {
-        // 1. If no root, set root and return.
+        // 1: If no root, set root and return.
         const newNode = new Node(key, value);
         if (!this.root) return (this.root = newNode);
 
-        // 2. Recursive traversal function.
+        // 2: Recursive traversal function.
         const traverse = (node) => {
-            // 2A. Returns if key's are equal. We don't want duplicates in the tree.
+            // 2A: Returns if key's are equal. We don't want duplicates in the tree.
             if (newNode.key === node.key) return;
-            // 2B. Left Case.
+
+            // 2B: Right Case. If there is no node at that spot, set node and return. Job done.
+            // ### If there is a node at that spot, call traverse and start again
             if (newNode.key > node.key) {
                 if (!node.right) return (node.right = newNode);
                 traverse(node.right);
             }
-            //2C. Right Case.
+
+            //2C: Same thing as right case but with the left.
             if (newNode.key < node.key) {
                 if (!node.left) return (node.left = newNode);
                 traverse(node.left);
             }
         };
-        // 3. Call recursive traversal function.
+        // 3: Call recursive traversal function.
         traverse(this.root);
     }
     /** Given a key, will search the tree and return the value at that key.
      * Utilizes recursion to traverse the tree.*/
     search(key) {
-        // 1. Recursive traversal function definition.
+        // 1: Recursive traversal function definition.
         const traverse = (node) => {
-            // 1A. Check if we found our desired key
+            // 1A: Check if we found our desired key
             if (node.key === key) return node.value;
-            // 1B. Left Case.
+
+            // 1B: Left Case.
             if (key < node.key) return traverse(node.left);
-            // 1C. Right Case
+
+            // 1C: Right Case
             if (key > node.key) return traverse(node.right);
         };
-        // 2. Call the recursize traversal function.
+        // 2: Call the recursize traversal function.
         return traverse(this.root);
     }
 }
