a4437b24197b37a51df40e269f32897a258422b8
Nick Raffaele <Raffaele.nickp@gmail.com>
2022-01-15 19:42:15 -0500

    Added search method to Binary Search Tree.

--- bst/bst.js
+++ bst/bst.js
@@ -4,29 +4,51 @@ class BinarySearchTree {
     constructor(node) {
         this.root = node;
     }
-
+    /** Given a key and value, will insert that key and it's associated value in the tree.
+     * Utilizes recursion to traverse the tree.*/
     insert(key, value) {
+        // 1. If no root, set root and return.
         const newNode = new Node(key, value);
         if (!this.root) return (this.root = newNode);
 
+        // 2. Recursive traversal function.
         const traverse = (node) => {
+            // 2A. Returns if key's are equal. We don't want duplicates in the tree.
             if (newNode.key === node.key) return;
+            // 2B. Left Case.
             if (newNode.key > node.key) {
                 if (!node.right) return (node.right = newNode);
                 traverse(node.right);
             }
+            //2C. Right Case.
             if (newNode.key < node.key) {
                 if (!node.left) return (node.left = newNode);
                 traverse(node.left);
             }
         };
+        // 3. Call recursive traveral function.
         traverse(this.root);
+    }
+    /** Given a key, will search the tree and return the value at that key.
+     * Utilizes recursion to traverse the tree.*/
+    search(key) {
+        // 1. Recursive traversal function definition.
+        const traverse = (node) => {
+            // 1A. Check if we found our desired key
+            if (node.key === key) return node.value;
+            // 1B. Left Case.
+            if (key < node.key) return traverse(node.left);
+            // 1C. Right Case
+            if (key > node.key) return traverse(node.right);
+        };
+        // 2. Call the recursize traversal function.
+        return traverse(this.root);
     }
 }
 
 const bst = new BinarySearchTree();
 bst.insert(5, 'hello');
-bst.insert(4, 'hello');
-bst.insert(3, 'hello');
-bst.insert(10, 'hello');
-console.log(bst);
+bst.insert(4, 'bar');
+bst.insert(3, 'foo');
+bst.insert(10, 'cat');
+console.log(bst.search(4));
